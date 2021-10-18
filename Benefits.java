08fbd5b93a990c2e8d1af4b51952de862bcafea7
merkeezy <92034316+merkeezy@users.noreply.github.com>
2021-10-18 10:12:10 -0400

    Add files via upload

---
+++ Benefits.java
@@ -0,0 +1,18 @@
+package com;
+
+import java.sql.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Benefits {
+    //constants
+    final double medicalInsurance = 316.88;
+    final double dentalInsurance = 228.42;
+    final double visionInsurance = 82.11;
+
+    //print values
+    private double salary = 0;
+    private double taxRate = 0;
+
+
+}
