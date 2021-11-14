bcfe3a56f7d6525c74a6f6d0761ced66c9d5c5fd
Tre'Vyon Bates <87159889+Tartpop9@users.noreply.github.com>
2021-11-13 19:35:47 -0600

    Update salary.java

--- java/src/com/TreVyon_Bates/salary.java
+++ java/src/com/TreVyon_Bates/salary.java
@@ -2,12 +2,10 @@ package com.TreVyon_Bates;
 
 public class salary
 {
-    static float wage;
-    static float hoursWorked;
+    static float grossYearlySalary
     static float taxes;
     public static float findSalary()
     {
-        float grossYearlySalary = wage * hoursWorked;
         float netYearlySalary = grossYearlySalary - taxes;
         System.out.println(netYearlySalary);
         return netYearlySalary;
